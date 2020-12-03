9257e5d9528161799dd920b481ff0cb76c0ca20d
Darkus <luisfrodriguezb.lf@gmail.com>
2020-12-03 18:52:11 -0400

    Solucionar esto

--- archivo.c
+++ archivo.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct _Datos
+{
+	int id;
+	int time;
+	int source;
+	int destination;
+	char protocol[3];
+	int lenght;
+	char description[50];
+};
+
+
+typedef struct _Nodo {
+   struct  _Datos info;
+   struct _Nodo *siguiente;
+} tipoNodo;
+
+typedef tipoNodo *punteroNodo;
+typedef tipoNodo *Lista;
 
 FILE *archivo;
 
@@ -17,11 +38,25 @@ int main(){
 	verificarArchivo(archivo);
 
 
-	char cadena[250];
-	while(fgets(cadena, 250, archivo) != NULL)
+	char cadena[250], cadena2[20];
+	int longuitud=250, i;
+
+	/*while(fgets(cadena, longuitud, archivo) != NULL)
 	{
-		printf("%s\n", cadena);
+		i = strlen(cadena);
+		printf("%s\n", cadena[i]);
+	}*/
+
+	i=0;
+	while(!feof(archivo)){
+		if (fgetc(archivo) != '\n')
+		{
+			printf("linea %i\n", i);
+		} else {
+			i++;
+		}
 	}
-    
+
+	fclose(archivo);
   return 0;
 }
